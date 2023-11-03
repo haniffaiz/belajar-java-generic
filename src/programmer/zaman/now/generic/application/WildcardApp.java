773490b0ad57aeddd14a65b83deea0821d1216fa
Hanif Faiz Hidayat <grey.hanif@gmail.com>
2023-11-03 19:03:27 +0700

    Wildcard

--- src/programmer/zaman/now/generic/application/WildcardApp.java
+++ src/programmer/zaman/now/generic/application/WildcardApp.java
@@ -1,4 +1,19 @@
 package programmer.zaman.now.generic.application;
 
+import programmer.zaman.now.generic.MyData;
+
+
+
 public class WildcardApp {
+    public static void main(String[] args) {
+
+        print(new MyData<Integer>(100));
+        print(new MyData<String>("Hanz"));
+        print(new MyData<MultipleConstraintApp.Manager>(new MultipleConstraintApp.Manager()));
+
+    }
+
+    public static void print(MyData<?> myData){
+        System.out.println(myData.getData());
+    }
 }
